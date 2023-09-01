ec1b7a01837e215e91ac668e5edcb8f64600d992
natalia <naty02jun@gmail.com>
2023-09-01 10:26:59 -0600

    add vars header #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+    name : string = "Natalia López Miranda";
+    titulo : string  = "ISOF";
+    objetivo : string = "Frontend Developer";
+    foto : string = "fake photo";
+    email : string = "t.natalialm@gmail.com";
+    celular : string = "2711355028";
+    ubicacion : string =  "Fortín, Veracruz";
+    redsocial : string = "NatLM";
 }
