aee7c69526af84ad76bc796b07dc5ca41f90cd3b
natalia <naty02jun@gmail.com>
2023-09-07 21:17:41 -0600

    ajustes en css

--- src/app/work_experience/work_experience.component.ts
+++ src/app/work_experience/work_experience.component.ts
@@ -17,37 +17,22 @@ export class WorkExperienceComponent implements OnInit{
   pais : string = "fake country";
 
   logros: any[] = [];
-  //data: [] = [];
-
-  /* ngOnInit(): void {
-    this.workExperienceService.getwork_experience()
-    .subscribe((data:any) => {
-      console.log(data[0]);
-      this.experience=data[0];
-    });
-  }
-} */
+  works: any[]=[];
 
   ngOnInit(): void {
     this.workExperienceService.getwork_experience()
     .subscribe( (data: any) => {
-      //console.log(data);
-      
-      this.puesto = data[0].puesto;
-      this.empresa=data[0].empresa;
-      this.fecha_ini=data[0].fecha_ini;
-      this.fecha_fin=data[0].fecha_fin;
-      this.ciudad=data[0].ciudad;
-      this.pais=data[0].pais;
-      this.logros=data[0].logros;
-     },
-     error => {
-       console.log("Error", error);
-   /*  this.workExperienceService = data[0];
-    this.workExperienceService = data[1]; */
-  /*   console.log(data[0]);
-    console.log(data[1]); */
-    } 
-    );
-  }
+      this.works = data.map((campo: any) => {
+        return {
+          puesto: campo.puesto,
+          empresa: campo.empresa,
+          fecha_ini: campo.fecha_ini,
+          fecha_fin: campo.fecha_fin,
+          ciudad: campo.ciudad,
+          pais : campo.pais,
+          logros: campo.logros
+        };
+      });
+    });
+  }
 }
