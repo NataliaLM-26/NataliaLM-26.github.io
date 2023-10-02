3185551440438bfc1797fa2193413d4287783293
NataliaLM-26 <142827054+NataliaLM-26@users.noreply.github.com>
2023-10-02 07:29:01 -0600

    Develop (#29)
    * create release hotfixes
    
    * create develop branch
    
    * Create develop_CI_CD.yml
    
    * Header Closes #3 (#8)
    
    * create mockup header #3
    
    * add vars header #3
    
    * cambios en header cv #3
    
    * fix test #3
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>
    
    * Work experience Closes #9 (#11)
    
    * primeros cambios en la seccion work_experience #2
    
    * primeros cambios en CSS
    
    * ajustes en css
    
    * ultima actualizacion de work_experience #9
    
    * Ultima modificacion #9
    
    * Cambio en pruebas
    
    * cambio en karma.conf.js #9
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>
    
    * Skills Closes #12 (#13)
    
    * create skills branch
    
    * incorporación en html y ts #12
    
    * correcion de errores #12
    
    * implementacion de css #12
    
    * finalizacion de skills #12
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>
    
    * Certificates Closes #14 (#15)
    
    * creacion de rama certificates, adision de componente y servicio #14
    
    * incorporacion de componente certificates y modificaciones en html, css y ts #14
    
    * Interests Closes #16 (#17)
    
    * incorporacion de componente y servicio de interests #16
    
    * cambios en html y css de interests #16
    
    * cierre en interests #16
    
    * sustitución de localhost por okteto de la api (#18)
    
    * Resolved merge conflict
    
    * Prueba (#25)
    
    * create release branch
    
    * Create release_ci_cd.yml
    
    * Create k8s.yml (#19)
    
    * Update k8s.yml (#23)
    
    * Resolved merge conflict
    
    * cambios para develop
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>
    
    * Pruebahot (#27)
    
    * create release hotfixes
    
    * create release branch
    
    * Create release_ci_cd.yml
    
    * Create k8s.yml (#19)
    
    * Update k8s.yml (#23)
    
    * Resolved merge conflict
    
    * prueba para hotfixes
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>
    
    * reset de release
    
    * Integrar (#30)
    
    * create release hotfixes
    
    * create release branch
    
    * Create release_ci_cd.yml
    
    * Create k8s.yml (#19)
    
    * Update k8s.yml (#23)
    
    * Resolved merge conflict
    
    * reset de release
    
    * Releaseprueba (#28)
    
    * reset de release
    
    * nueva rama para commit
    
    * rama nueva para hacer commit
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>
    
    ---------
    
    Co-authored-by: natalia <naty02jun@gmail.com>

---
+++ src/app/services/certificates.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CertificatesService {
+
+  apiURL = 'https://apicv-service-natalialm-26.cloud.okteto.net/certificates'
+  constructor(private http:HttpClient) { }
+
+  httpOptions ={
+    headers : new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+
+  getcertificates(): Observable<any>{
+    return this.http.get<any>(this.apiURL, this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(error:any){
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent){
+      //Get client-side error
+      errorMessage = error.error.message;
+      }else{
+        // Get server-side error
+        errorMessage= `Error Code: ${error.status}\n Message: ${error.message}`;
+  }
+
+  window.alert(errorMessage);
+  return throwError(errorMessage);
+  }
+
+}
